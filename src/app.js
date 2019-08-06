8b1259e77cd2ff0d37453d22858108e397e0d61d
Fidelma <fidelma.beagan@gmail.com>
2019-08-06 15:19:15 +0100

    total population and selecting country

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue ({
+    el: "#app",
+    data: {
+      worldData: [],
+      selectedCountry: null,
+      imageLink: null
+    },
+    mounted() {
+      this.getWorldData();
+    },
+    computed: {
+      totalPopulation: function () {
+        return this.worldData.reduce((sum, country) => {
+          return sum + country.population;
+        }, 0);
+      }
+    },
+    methods: {
+      getWorldData: function () {
+        const request = fetch("https://restcountries.eu/rest/v2/all")
+        .then(response => response.json())
+        .then(data => this.worldData = data)
+      },
+      displayCountry: function () {
+        this.imageLink = this.worldData[this.selectedCountry].flag;
+      }
+    }
+  })
+})
